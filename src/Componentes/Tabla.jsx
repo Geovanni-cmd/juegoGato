cb02bc0ca4598509d75580d3b87274a0e6c1acb8
Geovanni <carbajal.perez180320@gmail.com>
2024-12-30 16:17:33 -0600

    Eleccion de Ganador, creacion de matriz de patrones de ganar y desicion final

--- src/Componentes/Tabla.jsx
+++ src/Componentes/Tabla.jsx
@@ -6,10 +6,10 @@ const Tabla = () => {
     const [xEsSiguiente, setEsSiguiente] = useState(true);
     const [cuadros, setCuadros] = useState(Array(9).fill(null));
 
-    
+
 
     function handleClick(i){
-        if(cuadros[i]){
+        if(cuadros[i] || calculaGanador(cuadros)){
             return;
         }
         const siguienteCuadrado =  cuadros.slice();
@@ -22,8 +22,18 @@ const Tabla = () => {
         setEsSiguiente(!xEsSiguiente);
     }
 
+
+    const ganador = calculaGanador(cuadros);
+    let status;
+    if(ganador){
+        status = "Ganador: " + ganador;
+    } else {
+        status = "Siguiente Jugador: " + (xEsSiguiente ? "X" : "O");
+    }
+
     return (
         <>
+        <div className="status">{status}</div>
             <div className="board-row">
                 <Cuadrado value= {cuadros[0] } cuadradoClick={ () => handleClick(0)} />
                 <Cuadrado value= {cuadros[1] } cuadradoClick={ () => handleClick(1)}/>
@@ -42,5 +52,26 @@ const Tabla = () => {
         </>
     );
 }
+
+function calculaGanador(cuadros){
+    const lineas = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ];
+    for (let i = 0; i < lineas.length; i++){
+        const [a, b, c] = lineas[i];
+        if(cuadros[a] && cuadros[a] === cuadros[b] && cuadros[a] === cuadros[c]){
+            return cuadros[a];
+        }
+    }
+}
+
+
 
 export default Tabla;
